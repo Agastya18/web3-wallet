b2a26ba32d8ba4b32b8eec59a08f994d30f4359f
agastya gaur <agastyagaur17@gmail.com>
2024-11-20 19:29:16 +0530

    add web3 proejct

---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+import { CircleDollarSign } from "lucide-react";
+import React from "react";
+import { ModeToggle } from "./ui/theme-button";
+import { Separator } from "./ui/separator";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center py-4">
+      <div className="flex items-center gap-2">
+        <CircleDollarSign className="size-8" />
+        <div className="flex flex-col gap-4">
+          <span className="tracking-tighter text-3xl font-extrabold text-primary flex gap-2 items-center">
+            Kosh
+         
+          </span>
+        </div>
+      </div>
+      <ModeToggle />
+    </nav>
+  );
+};
+
+export default Navbar;
